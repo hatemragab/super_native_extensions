4611ca82e997f337812d1179e7225ff692a6661d
hatemragap <hatemragap5@gmail.com>
2022-11-03 15:51:47 +0200

    delete android and ios from the package

--- super_drag_and_drop/example/lib/main.dart
+++ super_drag_and_drop/example/lib/main.dart
@@ -8,8 +8,8 @@ import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:super_clipboard/super_clipboard.dart';
-import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 import 'package:super_clipboard_example/widget_for_reader.dart';
+import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -88,7 +88,9 @@ class _DragableWidgetState extends State<DragableWidget> {
   @override
   Widget build(BuildContext context) {
     return DragItemWidget(
-      allowedOperations: () => [DropOperation.copy],
+      allowedOperations: () => [
+        DropOperation.copy,
+      ],
       canAddItemToExistingSession: true,
       dragItemProvider: provideDragItem,
       child: DraggableWidget(
--- super_drag_and_drop/example/linux/flutter/generated_plugins.cmake
+++ super_drag_and_drop/example/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  super_native_extensions
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
